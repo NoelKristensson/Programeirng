16251ea5c0938582ad9a3df15fafb8d9b1e27001
Noel Kristensson <Noel Kristensson05@skola.stenungsund.se>
2023-09-18 10:19:52 +0200

    påbörjar

---
+++ 7040 in o utmatning/uppgift_1.py
@@ -0,0 +1,12 @@
+'''
+Skriv ett program som gör följande.
+
+Be användaren mata in sin favoritfärg.
+
+Be användaren att även mata in sitt namn.
+
+Gör en utskrift som blir ungefär så här.
+
+Rikard din favoritfärg är lila.
+'''
+print("hej")
